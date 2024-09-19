4749aef3eab3feda29d145cbc8c1d56e0a93ba30
mq7253180 <mq7253180@126.com>
2024-09-20 01:00:37 +0800

    xxx

--- springboot/src/main/java/com/honda/SpringBootConfiguration.java
+++ springboot/src/main/java/com/honda/SpringBootConfiguration.java
@@ -4,6 +4,7 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 import com.honda.freemarker.PaginationTemplateDirectiveModelBean;
 
@@ -11,9 +12,12 @@ import com.honda.freemarker.PaginationTemplateDirectiveModelBean;
 public class SpringBootConfiguration {
     @Autowired
     private freemarker.template.Configuration configuration;
+    @Autowired
+    private DefaultCookieSerializer cookieSerializer;
 
     @PostConstruct
     public void init() {
     	configuration.setSharedVariable("p", new PaginationTemplateDirectiveModelBean());
+    	cookieSerializer.setUseHttpOnlyCookie(false);
     }
 }
