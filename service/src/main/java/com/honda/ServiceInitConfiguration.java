0fac867ba41f1a49a10b889a9a34e5b7003f121f
mq7253180 <mq7253180@126.com>
2024-10-31 11:21:33 +0800

    xxx

--- service/src/main/java/com/honda/ServiceInitConfiguration.java
+++ service/src/main/java/com/honda/ServiceInitConfiguration.java
@@ -2,54 +2,25 @@ package com.honda;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.honda.service.UserService;
 import com.quincy.sdk.AuthActions;
-import com.quincy.sdk.Client;
 import com.quincy.sdk.RootControllerHandler;
 import com.quincy.sdk.TempPwdLoginEmailInfo;
-import com.quincy.sdk.o.User;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @PropertySource({"classpath:application-service.properties", "classpath:application-sensitiveness.properties"})
 @Configuration
 public class ServiceInitConfiguration {
-	@Autowired
-	private UserService userService;
-
 	@Bean
 	public AuthActions authActions() {
 		return new AuthActions() {
 			@Override
-			public User findUser(String username, Client client) {
-				return userService.find(username, client);
-			}
-
-			@Override
-			public void updateLastLogin(Long userId, String jsessionid, Client client) {
-				User vo = new User();
-				vo.setId(userId);
-				vo.setJsessionid(jsessionid);
-				userService.updateLogin(vo, client);
-			}
-
-			@Override
-			public void updatePassword(Long userId, String password) {
-				User vo = new User();
-				vo.setId(userId);
-				vo.setPassword(password);
-				userService.update(vo);
-			}
-
-			@Override
-			public ModelAndView signinView(HttpServletRequest request) {
-				return null;
+			public void loadAttributes(Long userId, Map<String, Object> attributes) {
+				
 			}
 		};
 	}
